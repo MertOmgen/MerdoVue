0583743930cf86078d2646d8cd30f18f4e025652
MertOmgen <mertomgen@gmail.com>
2020-12-18 17:43:36 +0300

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ import Vue from 'vue/dist/vue';
         var app = new Vue({
             el:"#app",
             data:{
-                message:"Hello Index"
+                message:"Hello Vue"
             }
         })
